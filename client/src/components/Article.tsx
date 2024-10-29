879ba6a92b02bb2803da9da8cd11fd5d0d75273a
JlCln <j.coulon.pro@gmail.com>
2024-10-29 12:23:12 +0100

    added colors for distance, needs fixing later

--- client/src/components/Article.tsx
+++ client/src/components/Article.tsx
@@ -16,10 +16,24 @@ function Article({ img, description, title, url, distance }: ArticleProps) {
       <div className="article-text">
         <h1>{title.charAt(0).toUpperCase() + title.slice(1)}</h1>
         <p>{description.charAt(0).toUpperCase() + description.slice(1)}</p>
-        {distance !== undefined && <p>Distance: {distance.toFixed(1)} km</p>}
+        {distance !== undefined && (
+          <p
+            className="distance-text"
+            style={{
+              color:
+                distance < 30
+                  ? "#99f2a5"
+                  : distance < 60
+                    ? "#eda77b"
+                    : "#db6060",
+            }}
+          >
+            Distance: {distance.toFixed(1)} km
+          </p>
+        )}
         {url && (
           <a className="details" href={url} target="_blank" rel="noreferrer">
-            View more details
+            Site Web
           </a>
         )}
       </div>
